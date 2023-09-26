07a338bb3a1e2aa1262d199ebaf6a2247b83f3f0
MonaHer <hertermona@gmail.com>
2023-09-26 23:39:48 +0200

    add favorite function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MealsList from "./features/meals/MealsList";
+import FavoritesList from "./features/favorites/FavoritesList";
 
 export default function App() {
   return (
     <div>
       <MealsList />
+      <FavoritesList />
     </div>
   );
 }
